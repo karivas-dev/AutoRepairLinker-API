5582782266018c2fbd571c00813c05fb7fa10a67
karivas-dev <karenrivasg17@gmail.com>
2023-08-20 22:57:22 -0600

    Fix column name

--- database/migrations/2023_08_20_031652_create_owners_table.php
+++ database/migrations/2023_08_20_031652_create_owners_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
             $table->string('telephone');
