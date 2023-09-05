d33b1c6c3370d3141207948dfa3cfdad0ceb59e2
Karen Rivas <karenrivasg17@gmail.com>
2023-09-05 15:15:31 -0600

    Work ticket and bid factories

--- database/factories/BidFactory.php
+++ database/factories/BidFactory.php
@@ -2,13 +2,17 @@
 
 namespace Database\Factories;
 
+use App\Models\BidStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Bid>
  */
 class BidFactory extends Factory
 {
+    static ?Collection $bid_Status_Ids = null;
+
     /**
      * Define the model's default state.
      *
@@ -16,7 +20,11 @@ class BidFactory extends Factory
      */
     public function definition(): array
     {
+        if (BidFactory::$bid_Status_Ids == null) {
+            BidFactory::$bid_Status_Ids = BidStatus::pluck('id');
+        }
         return [
+            'bid_status_id' => BidFactory::$bid_Status_Ids->random(),
             'budget' => fake()->randomFloat(2),
             'timespan' => fake()->dateTimeBetween('-10 years', '+10 years'),
         ];
