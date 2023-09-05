f23f05c740c16169cfc41fa241198e76e3fb8795
karivas-dev <karenrivasg17@gmail.com>
2023-09-04 23:51:52 -0600

    Update factory

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -2,13 +2,20 @@
 
 namespace Database\Factories;
 
+use App\Models\Insurer;
+use App\Models\Model;
+use App\Models\Owner;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Collection;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Car>
  */
 class CarFactory extends Factory
 {
+    static ?Collection $ownerIds = null;
+    static ?Collection $modelIds = null;
+    static ?Collection $insurerIds = null;
     /**
      * Define the model's default state.
      *
@@ -16,10 +23,22 @@ class CarFactory extends Factory
      */
     public function definition(): array
     {
+        if (CarFactory::$ownerIds == null) {
+            CarFactory::$ownerIds = Owner::pluck('id');
+        }
+        if (CarFactory::$modelIds == null) {
+            CarFactory::$modelIds = Model::pluck('id');
+        }
+        if (CarFactory::$insurerIds == null) {
+            CarFactory::$insurerIds = Insurer::pluck('id');
+        }
+
         return [
-            'model' => fake()->sentence(2),
             'plates' => fake()->word(),
-            'serial_number' => fake()->word()
+            'serial_number' => fake()->word(),
+            'owner_id' => CarFactory::$ownerIds->random(),
+            'model_id' => CarFactory::$modelIds->random(),
+            'insurer_id' => CarFactory::$insurerIds->random()
         ];
     }
 }
