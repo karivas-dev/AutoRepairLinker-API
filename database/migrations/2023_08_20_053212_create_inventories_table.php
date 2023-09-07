a902e1e644c77b27e2f13755c801454944fcab9b
karivas-dev <karenrivasg17@gmail.com>
2023-09-07 00:26:54 -0600

    Change column name

--- database/migrations/2023_08_20_053212_create_inventories_table.php
+++ database/migrations/2023_08_20_053212_create_inventories_table.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Models\Branch;
 use App\Models\Replacement;
-use App\Models\Store;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Store::class)->constrained();
+            $table->foreignIdFor(Branch::class)->constrained();
             $table->foreignIdFor(Replacement::class)->constrained();
             $table->integer('quantity');
             $table->decimal('unit_price');
